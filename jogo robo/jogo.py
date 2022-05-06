dd808669060d7a7cd2165953d6cd57dd00439d6d
Felipe alef <felipealef@gmail.com>
2022-05-06 12:28:30 -0300

    jogo completo

--- jogo robo/jogo.py
+++ jogo robo/jogo.py
@@ -1,7 +1,10 @@
+import random
 class Ponto:
     def __init__(self,x,y):
         self.x=x
         self.y =y
+    def __str__(self):
+        return "<%s,%s>" %(self.x,self.y)
 
 class Robo(Ponto):
 
@@ -30,14 +33,58 @@ class Recompensa(Ponto):
     def __init__(self, x, y,nome):
         super(Recompensa,self).__init__(x, y)
         self.nome=nome
+
+    def __str__(self):
+        return "<%s,%s>: %s" %(self.x,self.y,self.nome)
+
+    def __repr__(self):
+        return Ponto.__str__(self)
+
 def conferir_recompensa(robo, recompensas):
     ok=False
+
     for recompensa in recompensas:
+
         if recompensa.x == robo.x and recompensa.y == robo.y:
             print("O robô achou a recompensa " + recompensa.nome)
             ok = True
+
     return ok
-r1=Robo(2,10)
-reward1= Recompensa(2,10,'moeda')
-recompensas =[reward1]
-conferir_recompensa(r1,recompensas)
+
+r1 =Recompensa(random.randint(0,10), random.randint(0,10), 'moeda')
+r2 =Recompensa(random.randint(0,10), random.randint(0,10), 'gasolina')
+r3=Recompensa(random.randint(0,10), random.randint(0,10), 'arma')
+r4 =Recompensa(random.randint(0,10), random.randint(0,10), 'moeda')
+r5 =Recompensa(random.randint(0,10), random.randint(0,10), 'gasolina')
+r6=Recompensa(random.randint(0,10), random.randint(0,10), 'arma')
+
+rewards=[r1,r2,r3,r4,r5,r6]
+robo = Robo(random.randint(0,10),random.randint(0,10))
+for i in range(20):
+    print("O robo esta na posicao "+robo.__str__())
+    move=False
+    while move==False:
+        movimmento = input("Digite o movimento nas teclas w a s d: ").upper()
+        if movimmento == 'W':
+            robo.mover_pra_cima()
+            conferir_recompensa(robo,rewards)
+            move=True
+        elif movimmento == 'S':
+            robo.mover_pra_baixo()
+            conferir_recompensa(robo,rewards)
+            move=True
+        elif movimmento == 'A':
+            robo.mover_pra_esquerda()
+            conferir_recompensa(robo,rewards)
+            move = True
+        elif movimmento == 'D':
+            robo.mover_pra_direita()
+            conferir_recompensa(robo,rewards)
+            move=True
+        else:
+            print('Movimento invalido')
+            
+        
+    
+print("Fim de jogo, as recompensas estavam nas posições: %s" % rewards.__repr__())
+
